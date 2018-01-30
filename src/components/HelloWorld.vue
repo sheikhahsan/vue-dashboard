f6bb8f65e18fd42d6974618404564b04e5f3cd70
sheikhahsan <ravianfastian@gmail.com>
2018-01-30 22:26:06 +0500

    add element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="small title">{{ msg }}</h1>
+    <div>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+
   </div>
 </template>
 
